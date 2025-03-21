0f99c15c551f4bf60d719e6239803021af578b69
harshal211997 <harshal211997@gmail.com>
2025-03-21 14:44:40 +0530

    userRouter updated

--- router/userRouter.js
+++ router/userRouter.js
@@ -3,22 +3,34 @@ const userController = require("../controller/userController.js");
 const authController = require("../controller/authController.js");
 const router = express.Router();
 
-// Public Routes
+//Routers
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 
-// Protected Routes (Requires Authentication)
+//protect route:
 router.use(authController.protect);
+//
 
-// Admin-Only Routes
-router.use(authController.restrictTo("Admin"));
-router.put("/:userId/assignRole", userController.assignUserRole);
-router.get("/listUsers", userController.listUsers);
-router.delete("/:userId/deleteUser", userController.deleteUser);
-
-// Admin & User Routes
-router.use(authController.restrictTo("Admin", "User"));
+router.put(
+  "/:userId/assignRole",
+  authController.restrictTo("Admin"),
+  userController.assignUserRole
+);
+router.get(
+  "/listUsers",
+  authController.restrictTo("Admin"),
+  userController.listUsers
+);
 router.get("/:userId", userController.getUserById);
-router.put("/:userId/updateUser", userController.updateUser);
+router.put(
+  "/:userId/updateUser",
+  authController.restrictTo("User"),
+  userController.updateUser
+);
+router.delete(
+  "/:userId/deleteUser",
+  authController.restrictTo("Admin", "User"),
+  userController.deleteUser
+);
 
 module.exports = router;
